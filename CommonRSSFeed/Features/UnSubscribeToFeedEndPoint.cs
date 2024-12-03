5611d1082ace9486c6439095454bf7c39304c225
common khadka <commonkhadka@gmail.com>
2024-12-04 00:54:33 +0545

    Add Unsubscribe to feed endpoint

---
+++ CommonRSSFeed/Features/UnSubscribeToFeedEndPoint.cs
@@ -0,0 +1,37 @@
+﻿using CommonRSSFeed.DB;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace CommonRSSFeed.Features
+{
+    public class UnSubscribeToFeedEndPoint : Endpoint <UnSubscribeToFeedRequest>
+    {
+        private readonly AppDBContext _context;
+
+        public UnSubscribeToFeedEndPoint(AppDBContext context)
+        {
+            _context = context;
+        }
+
+        public override void Configure()
+        {
+            Post("api/feeds/unsubscribe");
+        }
+        public override async Task HandleAsync(UnSubscribeToFeedRequest req, CancellationToken ct)
+        {
+            try
+            {
+                await _context.Subscriptions.Where(a=>a.UserId == User.ToTokenUser().Id && a.FeedId == req.FeedId).ExecuteDeleteAsync();
+
+            }
+            catch (Exception ex)
+            {
+                ThrowError(ex.Message, StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+
+    public record UnSubscribeToFeedRequest(Guid FeedId);
+}
