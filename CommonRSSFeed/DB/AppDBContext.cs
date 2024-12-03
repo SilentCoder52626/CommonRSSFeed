ad7ed541cea90d34e48c0bc5f4bd161f55e47b67
common khadka <commonkhadka@gmail.com>
2024-12-04 00:50:38 +0545

    Fix Subscribtion migration and endpoint to add subscription

--- CommonRSSFeed/DB/AppDBContext.cs
+++ CommonRSSFeed/DB/AppDBContext.cs
@@ -11,8 +11,17 @@ namespace CommonRSSFeed.DB
         public DbSet<AppUser> AppUsers { get; set; }
         public DbSet<Feed> Feeds { get; set; }
         public DbSet<Post> Posts { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<AppUser>()
+                .HasMany(user => user.Feeds)
+                .WithMany(feed => feed.Users)
+                .UsingEntity<Subscription>();
+
+            modelBuilder.Entity<Subscription>()
+                .HasIndex(x=>new {x.FeedId,x.UserId}).IsUnique();
+
             base.OnModelCreating(modelBuilder);
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -28,6 +37,8 @@ namespace CommonRSSFeed.DB
         public required string Email { get; set; }
         public required string Password { get; set; }
         public required string Role { get; set; }
+        public List<Subscription> Subscriptions { get; set; } = new List<Subscription>();
+        public List<Feed> Feeds { get; set; } = new List<Feed>();
     }
     public class Feed
     {
@@ -36,6 +47,9 @@ namespace CommonRSSFeed.DB
         public required string Url { get; set; }
         public DateTime? LastFetchedAt { get; set; }
         public List<Post> Posts { get; set; } = new List<Post>();
+        public List<Subscription> Subscriptions { get; set; } = new List<Subscription>();
+        public List<AppUser> Users { get; set; } = new List<AppUser>();
+
     }
     public class Post
     {
@@ -47,6 +61,17 @@ namespace CommonRSSFeed.DB
 
         public required Guid FeedId { get; set; }
          public required Feed Feed { get; set; }
+
+    }
+
+    public class Subscription
+    {
+        public Guid Id { get; set; }
+        public required DateTime SubscribedAt { get; set; } = DateTime.UtcNow;
+        public Guid UserId { get; set; }
+        public AppUser User { get; set; }
+        public Guid FeedId { get; set; }
+        public Feed Feed { get; set; }
 
     }
 }
